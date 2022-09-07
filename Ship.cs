569dc046e78a8f90ee7bbaacd631b26b217074a6
jakub <username@example.com>
2022-09-07 21:15:28 +0200

    Add fields and constructor to Ship class

--- Ship.cs
+++ Ship.cs
@@ -2,6 +2,17 @@ namespace Battleships
 {
     class Ship
     {
-        Ship(int size, int[,] position, int[,] hit, bool sunk)
+        public int size;
+        public int[2] position;
+        public int direction;
+        public int[size] hit;
+        public bool sunk;
+
+        Ship(int size, int[2] position, int direction)
+        {
+            this.size = size;
+            this.position = position;
+            this.direction = direction;
+        }
     }
 }
